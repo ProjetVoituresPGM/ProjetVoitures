28710497c20d9666a970e9525b0ec1796bed6337
CedricMazet <mazet.cedric@gmail.com>
2014-11-21 10:32:05 +0100

    v1.1.0

--- Metier/Agence.cs
+++ Metier/Agence.cs
@@ -10,9 +10,39 @@ namespace Metier
     //agence cool caro
     public class Agence
     {
+        /// <summary>
+        /// Propriétés
+        /// </summary>
         private string nom;
         private string ParcVoitures;
         private List<Personne> lesPersonnes;
+
+
+        /// <summary>
+        /// Getters & Setters
+        /// </summary>
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public string ParcVoitures1
+        {
+            get { return ParcVoitures; }
+            set { ParcVoitures = value; }
+        }
+
+        internal List<Personne> LesPersonnes
+        {
+            get { return lesPersonnes; }
+            set { lesPersonnes = value; }
+        }
+
+        public Agence()
+        {
+
+        }
 
 
     }
