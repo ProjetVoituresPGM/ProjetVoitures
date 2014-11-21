7f31174ecd7b5d6ab6efbc13de450e0bcb0d9466
CedricMazet <mazet.cedric@gmail.com>
2014-11-21 10:26:58 +0100

    v1.0.7

---
+++ Metier/Personne.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    class Personne
+    {
+        /// <summary>
+        /// Propriétés
+        /// </summary>
+        private string nom;
+        private string prenom;
+        private string ville;
+
+        /// <summary>
+        /// Getters & Setters
+        /// </summary>
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        public string Ville
+        {
+            get { return ville; }
+            set { ville = value; }
+        }
+
+        /// <summary>
+        /// constructeur de personne
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="ville"></param>
+        public Personne(string nom, string prenom, string ville)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.ville = ville;
+        }
+
+
+        /// <summary>
+        /// surcharge du constructeur
+        /// </summary>
+        public Personne()
+        { }
+
+        //Methode
+    }
+}
