cfa0ac5c3f6f40bd28dd8e20f7015f68ce6a5c4e
apeff <anais.pefferkorn@gmail.com>
2014-11-28 11:03:47 +0100

    ajout frmrendre

---
+++ Presentation/FrmRendreUneVoiture.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Presentation
+{
+    partial class FrmRendreUneVoiture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_nomLoueur = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_rendre = new System.Windows.Forms.ComboBox();
+            this.btnFermer = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(119, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rendre une voiture";
+            // 
+            // tbx_nomLoueur
+            // 
+            this.tbx_nomLoueur.Location = new System.Drawing.Point(211, 173);
+            this.tbx_nomLoueur.Name = "tbx_nomLoueur";
+            this.tbx_nomLoueur.Size = new System.Drawing.Size(121, 20);
+            this.tbx_nomLoueur.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nom du loueur :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Voiture à rendre :";
+            // 
+            // cbx_rendre
+            // 
+            this.cbx_rendre.FormattingEnabled = true;
+            this.cbx_rendre.Location = new System.Drawing.Point(211, 127);
+            this.cbx_rendre.Name = "cbx_rendre";
+            this.cbx_rendre.Size = new System.Drawing.Size(121, 21);
+            this.cbx_rendre.TabIndex = 4;
+            // 
+            // btnFermer
+            // 
+            this.btnFermer.Location = new System.Drawing.Point(124, 240);
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(75, 23);
+            this.btnFermer.TabIndex = 5;
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.UseVisualStyleBackColor = true;
+            this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(257, 240);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 6;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // FrmRendreUneVoiture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(499, 304);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.btnFermer);
+            this.Controls.Add(this.cbx_rendre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx_nomLoueur);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmRendreUneVoiture";
+            this.Text = "FrmRendreUneVoiture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_nomLoueur;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbx_rendre;
+        private System.Windows.Forms.Button btnFermer;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
