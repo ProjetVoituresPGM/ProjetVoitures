574fd31bedb3e03f529854345a681d9e15f5f278
apeff <anais.pefferkorn@gmail.com>
2014-11-28 10:39:08 +0100

    frm

--- Presentation/FrmSuppPersonne.Designer.cs
+++ Presentation/FrmSuppPersonne.Designer.cs
@@ -87,6 +87,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(561, 278);
             this.Controls.Add(this.btnSupp);
             this.Controls.Add(this.btnFermer);
